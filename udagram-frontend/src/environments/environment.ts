0f5a096f3cd44468a630d2c335be9194a3598d9a
nguyenan-nice <nguyenan.it88@gmail.com>
2023-11-10 17:48:16 +0700

    change host api

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://af5d9ecc8088e472fa843f7f3317f7d6-1735359839.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
